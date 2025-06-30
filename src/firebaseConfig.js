858a1e93130c367a73c11f8f54c4ba0140ababfe
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2025-06-30 20:44:39 -0300

    9. Clase 8 - Agregando variables para Firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { FIREBASE_API_KEY, FIREBASE_APP_ID, FIREBASE_AUTH_DOMAIN, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET } from "../variables";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4w26ZtQ6F32YEaKT1xotM4F49VoUGm-c",
-    authDomain: "react-73705.firebaseapp.com",
-    projectId: "react-73705",
-    storageBucket: "react-73705.firebasestorage.app",
-    messagingSenderId: "776419515667",
-    appId: "1:776419515667:web:1b1f336e0e454326964ee6"
-};
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig);
 
